79f2aeb8de6f70074788db0eeda0026cbabfcdbd
chomado <chomado@gmail.com>
2017-04-15 01:08:07 +0900

    Init

---
+++ test170415/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace test170415
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
