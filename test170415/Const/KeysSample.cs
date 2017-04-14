8f21a4c1e24b05f6158b8559b1fffa2c67aa2009
chomado <chomado@gmail.com>
2017-04-15 01:25:12 +0900

    鍵の文字列を格納するクラスを作った

---
+++ test170415/Const/KeysSample.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace test170415.Const
+{
+    public static class KeysSample
+    {
+        // Mobile Center の iOSアプリの AppSecret文字列
+        public static readonly string MobileCenterSecretIos = "aaaa-aaaa-aaaa-aaa-aaaaaaa";
+
+        // Mobile Center の Android アプリの AppSecret文字列
+        public static readonly string MobileCenterSecretAndroid = "aaaaa-aaa-aaaa-aaaa-aaaaaaaaaaaaaa";
+    }
+}
